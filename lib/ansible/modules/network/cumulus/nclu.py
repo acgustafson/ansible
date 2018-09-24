0530f28d287daf0bc006e6c79340640064c51a12
agustafson <andy.gustafson1@gmail.com>
2018-09-24 12:58:28 -0500

    Updated delimiter.

--- lib/ansible/modules/network/cumulus/nclu.py
+++ lib/ansible/modules/network/cumulus/nclu.py
@@ -167,7 +167,7 @@ def check_pending(module):
     """Check the pending diff of the nclu buffer."""
     pending = command_helper(module, "pending", "Error in pending config. You may want to view `net pending` on this target.")
 
-    delimeter1 = "net add/del commands since the last 'net commit'"
+    delimeter1 = 'net add/del commands since the last "net commit"'
     color1 = '\x1b[94m'
     if delimeter1 in pending:
         pending = pending.split(delimeter1)[0]
